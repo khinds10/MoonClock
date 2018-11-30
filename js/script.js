c0379adae2b05da9e0ee446774597a4d37a5cb0e
Kevin Hinds <khinds10.work@gmail.com>
2018-11-30 04:06:38 +0000

    added am/pm, styles fixed for tablet

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@
 function startTime() {
     var today = new Date();
     var h = today.getHours();
-    if (h > 12) h = h - 12;
+
+
+    var ampm = 'AM';
+    if (h > 12) {
+	h = h - 12;
+	ampm = 'PM';
+    }
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m;
+    document.getElementById('time').innerHTML = h + ":" + m + "<span id='ampm'>" + ampm + "</span>";
     var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};
     return i;
 }
